20598c5a361e8948824b974ff94b9911f372fe21
KrazyTom <karanrana5521@gmail.com>
2025-11-11 14:09:00 +0530

    Logs changes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,4 @@ app.get('/', (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
