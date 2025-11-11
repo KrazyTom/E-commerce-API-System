bd5136324af1891f9c6c20e781483cefafd629f4
KrazyTom <karanrana5521@gmail.com>
2025-11-11 14:41:37 +0530

    Register And Login APIs

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 import expess from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
-
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 const app = expess();   
 app.use(cors());
 app.use(expess.json()); 
 
+
+
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Welcome to Express + MYSQL E-commerce API System');
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
