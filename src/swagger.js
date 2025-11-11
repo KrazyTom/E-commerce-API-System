53d8c103b9e988b13240f8ef660949184a574acd
KrazyTom <karanrana5521@gmail.com>
2025-11-11 18:03:38 +0530

    Swagger Setup

---
+++ src/swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'E-commerce API',
+    version: '1.0.0',
+    description: 'API documentation for your E-commerce project',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Local server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const setupSwagger = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
